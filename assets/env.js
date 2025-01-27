73172155001dfbbf257ceeeb77c197e203fc46fc
Camilo Calderón <cristian.calderontapia@idartes.gov.co>
2025-01-26 20:45:58 -0500

    Auto-generated commit

--- assets/env.js
+++
@@ -1,4 +0,0 @@
-window.env = {
-    VITE_SUPABASE_URL: 'https://xcjzosuyzncqbcemljgy.supabase.co',
-    VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhjanpvc3V5em5jcWJjZW1samd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MzAyMDYsImV4cCI6MjA1MzUwNjIwNn0.V0wABlmJNK8uwwduEBkuI7jnNnVavNVkI2Wtja71LiM'
-};
